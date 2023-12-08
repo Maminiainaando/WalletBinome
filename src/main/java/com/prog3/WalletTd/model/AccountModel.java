6f9b4edb64c2665263fffd769bb78334d1d12ab7
ManoaRatsimandresy <hei.manoa.3@gmail.com>
2023-12-08 15:10:31 +0300

    feat: adding models

---
+++ src/main/java/com/prog3/WalletTd/model/AccountModel.java
@@ -0,0 +1,12 @@
+package com.prog3.WalletTd.model;
+
+public class AccountModel {
+    private int id;
+
+    private String nom;
+
+    private double solde_montant;
+
+    private Timestamp solde_date_maj;
+
+}
--- src/main/java/com/prog3/WalletTd/model/Currency.java
+++
@@ -1,5 +0,0 @@
-package com.prog3.WalletTd.model;
-
-public class Currency {
-    
-}
